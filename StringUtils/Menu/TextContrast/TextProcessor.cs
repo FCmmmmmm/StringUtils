4498829a09d5df72a4d446c9f84426cea31fb73a
FCmmmmmmm <1655887222@qq.com>
2019-03-04 21:34:16 +0800

    新增字符串处理块

---
+++ StringUtils/Menu/TextContrast/TextProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtils.Menu.TextContrast
+{
+    class TextProcessor
+    {
+        
+        public static string GetDifferent(string left,string right)
+        {
+            
+            return "";
+        }
+    }
+}
